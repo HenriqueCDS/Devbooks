8a978de0d8b0641ba38346d3a5004dcdc89d9ff1
henrique <henriquecordeiro054@gmail.com>
2024-01-04 17:47:20 -0300

    upload

---
+++ src/componentes/OpcoesHeader/index.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+const Opcao = styled.li`
+    font-size: 16px;
+    font-color:#EB9B00;
+    display: flex;
+    text-decoration:none;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 100%;
+    padding: 0 5px;
+    cursor: pointer;
+    min-width: 120px;
+`
+
+const Opcoes = styled.ul`
+    display: flex;
+`
+
+const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'ESTANTE']
+
+function OpcoesHeader() {
+    return (
+        <Opcoes>
+            { textoOpcoes.map( (texto) => (
+                <Link to={`/${texto.toLowerCase()}`} ><Opcao><p>{texto}</p></Opcao></Link>
+            ) ) }
+      </Opcoes>
+    )
+}
+
+export default OpcoesHeader
